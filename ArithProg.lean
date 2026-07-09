b38232a1ec62ad161e4275258e42c10ed6b3ee0a
chuckgarcian <Chuckgarcian@gmail.com>
2026-07-09 12:05:33 -0500

    Updated file description and references

--- ArithProg.lean
+++ ArithProg.lean
@@ -1,3 +1,7 @@
+-- Descripition: Defines an arithemtic progression object and then proves basic properties about it.
+-- Credit: Class implementation is followed from the link below. The lean4 proofs are all written by me
+-- Link: https://www.youtube.com/watch?v=84Gx0XbqQq8&t=3006s
+
 import Mathlib.Data.Real.Basic
 import Mathlib.Data.Nat.Basic
 import Mathlib.Tactic.Cases
