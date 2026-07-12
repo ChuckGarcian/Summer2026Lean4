fceac2f3461f157c16e3ef5a987e9f2f8996f2cb
chuckgarcian <Chuckgarcian@gmail.com>
2026-07-11 23:58:57 -0500

    Finish polynomial thm 4.6 backward direction

--- Polynomials/polynomials.lean
+++ Polynomials/polynomials.lean
@@ -7,6 +7,7 @@ import Mathlib.Data.Real.Basic
 import Mathlib.Algebra.Polynomial.Basic
 import Mathlib.Algebra.Polynomial.Degree.Defs
 import Mathlib.Algebra.Polynomial.Eval.Defs
+import Mathlib.Algebra.Polynomial.Laurent
 
 
 open Polynomial
@@ -14,9 +15,16 @@ universe u
 variable (α : Type u)
 variable (R : Type*) [CommRing R] (P Q: R[X]) (x : R)
 
+
 -- Syntatic sugar macros
 local notation "p" x:max => P.eval x
 local notation "q" x:max => Q.eval x
+
+
+#check LaurentPolynomial R
+#check (X : R[X]).toLaurent
+#check Polynomial.degree
+
 
 /-
 Theorem 4.6 - Each zero of a polynomial corresponds to a degree-one factor:
@@ -34,11 +42,29 @@ example
 
   -- (l : R) : P.eval l = 0 ↔ ∀ z : R, P.eval z = (z - l) * Q.eval z := by
     apply Iff.intro
+
+    -- Suppose l is a root. That is p(l) = 0
     case mp =>
-      intro h z
+      intro h
+
+
+
 
     -- Suppose there is poly q ∈ R[x] s.t degree(q) = m-1 and 𝑝(𝑧) = (𝑧 − 𝜆)𝑞(𝑧)
     case mpr =>
       intro h1
+      obtain ⟨polyq, assumq⟩ := h1
 
-      obtain ⟨q, hq⟩ := h1
+      -- Plug in root into the polynomial then show its zero
+      have h := assumq.right l
+      simp at h
+      exact h
+
+
+/-
+Theorem 4.8 - Degree 𝑚 implies at most 𝑚 zeros
+Suppose 𝑚 is a positive integer and 𝑝 ∈ 𝒫(𝐅) is a polynomial of degree 𝑚. Then
+𝑝 has at most 𝑚 zeros in 𝐅
+-/
+
+-- example
---
+++ Polynomials/polynomials.md
@@ -0,0 +1,29 @@
+
+**Claim:**
+
+$$\begin{equation}p(z) = (z - \lambda)q(z)\end{equation}$$
+
+**Proof:**
+Forward Direction: Suppose $\lambda$ is a root of $p$. This means $p(\lambda) = 0$
+
+We want to show there exists a polynomial of, $q \in R[X]$, of degree $m-1$, such that
+
+$$\begin{equation} p(z) = (z - \lambda)q(z)\end{equation}$$
+
+Let $T(k)$ be the statement
+
+**Basecase:** $T(1)$
+
+Suppose $p$ is a degree 1 polynomial. Then $p(x) = ax$ where $a$ is some constant in $R$.
+
+Observe, that 
+
+$T(k)$
+
+T(k+1)
+
+Assume a $k$ degree polynomial $p$ can be written 
+
+$p(z) = (z - \lambda)q(z) + r (z)$
+
+$p(z) - r(z) = (z - \lambda) q(z)$
