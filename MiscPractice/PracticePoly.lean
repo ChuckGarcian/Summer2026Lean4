473930345349caa05ecd33c626ecae0e738bcbab
chuckgarcian <Chuckgarcian@gmail.com>
2026-07-13 17:05:43 -0500

    Add more polynomial practice

--- MiscPractice/PracticePoly.lean
+++ MiscPractice/PracticePoly.lean
@@ -9,20 +9,27 @@ open Polynomial Finset
 
 variable (R : Type*) [CommRing R]
 
--- or all a b : R, the polynomial a·X² + b·X + 3 in R[X],
--- evaluated at 1, equals a + b + 3.
+
+/-
+For all a b : R, the polynomial a·X² + b·X + 3 in R[X], evaluated at 1,
+equals a + b + 3.
+-/
 example (a b : R) : (a • X^2 + b • X + 3 : R[X]).eval 1 = a + b + 3 := by
   simp
 
 
--- The square of a ring element is equivalent to the ring element together with itself
--- under the ring operation
+/-
+The square of a ring element is equivalent to the ring element together with
+itself under the ring operation
+-/
 example (G : Type*) [CommRing G] (a : G) : a^2 = a * a := by
   exact pow_two a
 
--- for all a b : R, the constant polynomial C (a * b²)
--- equals C a * C b * C b in R[X].
 
+/-
+for all a b : R, the constant polynomial C (a * b²) equals C a * C b * C b
+in R[X].
+-/
 example (a b : R) : C (a * b^2) = C a * C b * C b:= by
   rw [map_mul]
   conv =>
@@ -36,8 +43,8 @@ example (a b : R) : C (a * b^2) = C a * C b * C b:= by
   ring
 
 
-
 #check dvd_def
+
 
 -- for all a b c : R, if a ∣ b then a ∣ b * c.
 example {a b c : R} (h : a ∣ b) : a ∣ (b * c) := by
@@ -51,4 +58,3 @@ example {a b c : R} (h : a ∣ b) : a ∣ (b * c) := by
   case h =>
       rw[hx]
       ring
-
