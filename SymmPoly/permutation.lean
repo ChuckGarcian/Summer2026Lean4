72f00a6a2d7b9c5900855a901f84f7cb699e3987
chuckgarcian <Chuckgarcian@gmail.com>
2026-07-18 11:28:04 -0500

    Add symmetrization def

--- SymmPoly/permutation.lean
+++ SymmPoly/permutation.lean
@@ -4,51 +4,59 @@ import Mathlib.RingTheory.MvPolynomial.Symmetric.Defs
 import Mathlib.Algebra.MvPolynomial.Variables
 import Mathlib.Data.Fintype.Perm
 import Mathlib.Data.Finset.Image
+import Mathlib.Data.Real.Basic
+import Mathlib.Data.Nat.Factorial.Basic
+import Mathlib.Tactic.Explode
 
+-- set_option trace.Meta.synthInstance true
 open BigOperators
 open Finset
 
-variable (R : Type*) [CommSemiring R]
+
+/- Define global variables -/
 variable (σ τ : Type*)
-
--- variable (fnset : Finset σ)
-
 variable [Fintype σ] [DecidableEq σ]
 variable [Fintype τ] [DecidableEq τ]
+variable (p : MvPolynomial σ Real)
 
-variable (p : MvPolynomial σ R)
--- variable (f : Equiv.Perm σ → σ)
-
--- set_option trace.Meta.synthInstance true
-
-variable {α β : Type*} [AddCommMonoid β] [Fintype α] [DecidableEq α]
-variable (g : Equiv.Perm α → β)
-
--- #check ∑ σ : Equiv.Perm α, g σ
-#check Finset.sum Finset.univ g
-
--- def mapperm : σ → τ := fun (x : σ)
-variable (π : Equiv.Perm σ)
-#check π
-
-#check MvPolynomial.rename π p
 
 /-
 Let p be a symmetric multivariate polynomial; then p is invariant
 under any permutation of its variables.
 -/
-example : (MvPolynomial.IsSymmetric p) → (MvPolynomial.rename π p  = p) := by
-  exact fun a ↦ MvPolynomial.ext ((MvPolynomial.rename ⇑π) p) p fun m ↦ congrArg (MvPolynomial.coeff m) (a π)
+theorem symmInv
+  (π : ↑(Equiv.Perm σ))
+  : (MvPolynomial.IsSymmetric p) → (MvPolynomial.rename π p  = p) := by
+  exact fun a ↦ MvPolynomial.ext ((MvPolynomial.rename π) p) p fun m ↦ congrArg (MvPolynomial.coeff m) (a π)
 
-#check Finset.sum Finset.univ fun π ↦ MvPolynomial.rename π p
 
-def test := Finset.sum
+#explode symmInv
+#check Finset.sum Finset.univ fun π ↦ MvPolynomial.rename π p -- Symmetrization
 
-noncomputable def fns (P : MvPolynomial σ ℤ)  : MvPolynomial σ ℤ  := MvPolynomial.rename π P
 
-#check (fns p _)
-  -- dbg_trace f!"Current value of x: {x}"
+variable (fn := fun (q : MvPolynomial σ Real) (π : ↑(Equiv.Perm σ)) ↦ MvPolynomial.rename π q)
+variable (N := p.vars.card) -- Number of variables in a multivarirate poly
 
+
+/- Sanity Checks -/
+#check Finset.sum
+#check Finset.sum Finset.univ (fn p) -- Summation over all permutations of the polynomial p
+#check Finset.card
+
+#check N
+#check Nat.factorial N
+#check  (Finset.sum Finset.univ (fn p)) * MvPolynomial.C ((1.0:Real) / N.factorial)
+
+
+/-
+Let p: Rᴺ ↦ R be a polynomial, then $L(p)(x) = \frac{1}{N!}\sum_{\pi \in S_n}
+p(x\circ \pi)$ is the symmtization of $p$
+-/
+noncomputable def symmetrization : MvPolynomial σ Real :=
+  (Finset.sum Finset.univ (fn p)) * MvPolynomial.C ((1.0:Real) / N.factorial)
+
+
+/- Sanity Checks-/
+#check symmetrization
 #check Fintype
 #reduce Fintype
-
