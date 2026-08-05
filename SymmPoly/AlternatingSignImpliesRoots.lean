0c9b41bafc7e8cbc75cacbccc1dc49904f4326e8
chuckgarcian <Chuckgarcian@gmail.com>
2026-08-04 21:48:43 -0500

    Update Finset to fin N

--- SymmPoly/AlternatingSignImpliesRoots.lean
+++ SymmPoly/AlternatingSignImpliesRoots.lean
@@ -18,31 +18,31 @@ open Set
 #check intermediate_value_univ₂ hf hg
 #check intermediate_value_univ₂ hf hg ha hb
 
-/- Example: Let the g(x) = 0 and f(x) be a continous function such that
+/- Example: Let g(x) = 0 and f(x) be a continuous function such that
 for some x and y we have that f(x) < g(x) and g(y) < f(y). Then there must
 be some value, c ∈ ℝ, such that f(c) = 0.
 -/
 lemma thm0
   (x y : ℝ)
   (g : ℝ → ℝ) (hg : Continuous g) (hgz : ∀ a, g a = 0)
-  (f : ℝ → ℝ ) (hf : Continuous f)
+  (f : ℝ → ℝ) (hf : Continuous f)
   (ha : f x ≤ 0)
-  (ha  : f x ≤ g x)
-  (hb  : g y ≤ f y)     :
-  ∃ x  : ℝ, f x = 0:= by
+  (ha : f x ≤ g x)
+  (hb : g y ≤ f y) :
+  ∃ x : ℝ, f x = 0 := by
 
-    -- Use special case of intermediate value theorem
-    -- Obtain proof of root exists using IVU
+    -- Use special case of the intermediate value theorem
+    -- Obtain proof that a root exists using IVU
     obtain ⟨w, h⟩ := intermediate_value_univ₂ hf hg ha hb
 
-    -- IVU gives witness in terms of the zero function, g. So we rewrite in terms of 0 ∈ ℝ
+    -- IVU gives a witness in terms of the zero function, g. So we rewrite in terms of 0 ∈ ℝ
     rw [hgz w] at h
     apply Exists.intro
     exact h
 
 def zeroFunc : ℝ → ℝ := fun _ => 0
 
-/- Example: Let f(x) be a continous function such that
+/- Example: Let f(x) be a continuous function such that
 for some x and y we have that f(x) < 0 and 0 < f(y). Then there must
 be some value, c ∈ ℝ, such that f(c) = 0.
 -/
@@ -53,22 +53,20 @@ theorem thm1
   (hb : 0 ≤ f y) :
   ∃ x : ℝ, f x = 0 := by
 
-    -- Rewerite 0 constants as zero function: g(y) = 0, y ∈ ℝ
+    -- Rewrite the 0 constants as the zero function: g(y) = 0, y ∈ ℝ
     change f x ≤ zeroFunc x at ha
     change zeroFunc y ≤ f y at hb
 
-    -- Certicate showing zerofunction is continous and always equal to zero
+    -- Certificate showing the zero function is continuous and always equal to zero
     have hg : Continuous zeroFunc := by exact continuous_of_const fun x ↦ congrFun rfl
     have hgz : ∀ l, zeroFunc l = 0 := by exact fun l ↦ Real.ext_cauchy rfl
 
-    -- Reduced to thm0 case
+    -- Reduced to the thm0 case
     apply thm0; repeat' assumption
-
-
 
 #check LE.le.trans
 
-/- Example: Let f(x) be a continous function such that
+/- Example: Let f(x) be a continuous function such that
 for some real numbers x and y, we have that f(x) < -⅙ and ⅙ < f(y). Then there must
 be some real value, c, such that f(c) = 0.
 -/
@@ -77,16 +75,16 @@ theorem thm2
   (f : ℝ → ℝ)
   (hf : Continuous f)
   (ha : f x ≤ -1/6)
-  (hb: 1/6 ≤ f y) :
+  (hb : 1/6 ≤ f y) :
   ∃ x : ℝ, f x = 0 := by
 
-    -- Reduce statement as the more general statement at f(x) ≤ 0 and f(x) ≥ 0; This should be trivial -- emphasis on should :(
+    -- Reduce the statement to the more general statement at f(x) ≤ 0 and f(x) ≥ 0; this should be trivial -- emphasis on should :(
     have lowerBoundConv : (-1 : ℝ) / 6 ≤ 0 := by norm_num
-    have upperBoundConv : (1/6 : ℝ) ≥ 0  := by norm_num
-    have h1 : f x  ≤ 0 := by exact LE.le.trans ha lowerBoundConv
-    have h2 :  0 ≤ f y := by exact Std.IsPreorder.le_trans 0 (1 / 6) (f y) upperBoundConv hb
+    have upperBoundConv : (1/6 : ℝ) ≥ 0 := by norm_num
+    have h1 : f x ≤ 0 := by exact LE.le.trans ha lowerBoundConv
+    have h2 : 0 ≤ f y := by exact Std.IsPreorder.le_trans 0 (1 / 6) (f y) upperBoundConv hb
 
-    -- Thoerem one can prove the rest
+    -- Theorem one can prove the rest
     apply thm1; repeat' assumption
 
 #eval Finset.range 1
@@ -97,25 +95,21 @@ section
   #check ∀ x : Fin 3, (Even x ∧ f x.val ≤ -1/6) ∨ (Odd x.val ∧ f x ≥ 1/6)
 end section
 
-
 def isRoot (f : ℝ → ℝ) (x : ℝ) : Prop := f x = 0
-
 
   -- apply Exists.intro
   -- case w =>
---
+  --
   -- case h =>
-
-
 
 lemma base1
   (f : ℝ → ℝ) (hf : Continuous f)
-  (h2 :  ∀ x ∈ Finset.range 1, (Even x ∧ f x ≤ -1/6) ∨ (Odd x ∧ f x ≥ 1/6)) :
+  (h2 : ∀ x ∈ Finset.range 1, (Even x ∧ f x ≤ -1/6) ∨ (Odd x ∧ f x ≥ 1/6)) :
   ∃ s : Finset ℝ, s.card = 1 ∧ ∀ x ∈ s, isRoot f x := by sorry
 
 example
   (f : ℝ → ℝ) (hf : Continuous f)
-  (h2 :  ∀ x ∈ Finset.range 2, (Even x ∧ f x ≤ -1/6) ∨ (Odd x ∧ f x ≥ 1/6)) :
+  (h2 : ∀ x ∈ Finset.range 2, (Even x ∧ f x ≤ -1/6) ∨ (Odd x ∧ f x ≥ 1/6)) :
   ∃ s : Finset ℝ, s.card = 2 ∧ ∀ x ∈ s, isRoot f x := by
 
 Set.forall_mem_insert
@@ -145,9 +139,8 @@ lemma lemma0
   (hf : Continuous f) :
   ∀ x : Fin (N + 1), P f x ↔ P f (N + 1) ∧ ∀ x : Fin (N), P f x := by sorry
 
-
 variable (N : Nat)
-variable  (f : ℝ → ℝ) (hf : Continuous f)
+variable (f : ℝ → ℝ) (hf : Continuous f)
 
 #check base0
 #check base0 f
@@ -166,7 +159,7 @@ example
   ∀ x : Fin N, P f x → ∃ s : Finset ℝ, s.card = N ∧ ∀ x ∈ s, isRoot f x := by
   intro x hp
   induction' N with k hk
-  -- TODO: This will need to be made strong induction since we need the both cases of N = 0 AND N = 1
+  -- TODO: This will need to be made strong induction since we need both cases of N = 0 AND N = 1
   case zero =>
     exact base0 f hf x hp
 
@@ -179,16 +172,13 @@ example
     have pL := hfpx.left
     have pR := hfpx.right
 
-    -- Get witness set for card k. Now it remains to be shown the singlenton set exists
+    -- Get the witness set for card k. Now it remains to be shown that the singleton set exists
     apply helper1 at hk
 
     -- TODO: Once we get
     have s := hk pR
 
-
-
-
-
     -- apply lemma0
 
-    rw [Finset.forall_mem_insert (by decide) ] at h2
+    rw [Finset.forall_mem_insert (by decide)] at h2
+    sorry
